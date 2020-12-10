c516607f393b63ff5b7a0a94a19ff7a50c42b0f9
AGiantSquid <ashley.e.shultz@gmail.com>
2020-12-09 20:31:47 -0800

    update string utils

--- src/python_utils/string_utils.py
+++ src/python_utils/string_utils.py
@@ -7,13 +7,13 @@ import re
 DETECT_CAMEL_RE = re.compile(r'[A-Z]?[a-z]+|[A-Z]{2,}(?=[A-Z][a-z]|\d|\W|$)|\d+')
 
 
-def camel_to_snake(camel_str):
+def camel_to_snake(camel: str) -> str:
     '''Convert a camel cased string to snake case.'''
-    words = DETECT_CAMEL_RE.findall(camel_str)
+    words = DETECT_CAMEL_RE.findall(camel)
     return '_'.join(map(str.lower, words))
 
 
-def snake_to_camel(snake_str):
+def snake_to_camel(snake: str) -> str:
     '''Convert a snake cased string to camel case.'''
-    first, *others = snake_str.split('_')
+    first, *others = snake.split('_')
     return ''.join([first.lower(), *map(str.title, others)])

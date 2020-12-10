ad60988a78eafc8a150cab5ebecd85004d13b759
AGiantSquid <ashley.e.shultz@gmail.com>
2020-12-09 20:30:45 -0800

    refactor iterable utils

--- src/python_utils/iterable_utils.py
+++ src/python_utils/iterable_utils.py
@@ -6,19 +6,20 @@ from functools import reduce
 from typing import Any, Callable, Iterable, Tuple
 
 
-def divide(subject: Iterable, filter_func: Callable[[Any], bool]):
+# Tuple that can have any length
+VaribleTuple = Tuple[Any, ...]
+
+
+def partition(subject: Iterable, filter_func: Callable[[Any], bool]) -> Tuple[VaribleTuple, VaribleTuple]:
     '''Divide an iterable into 2 tuples according to filter func.
 
     The first tuple contains all elements that match the filter,
-    the second contains those that do not.
-    '''
-    # Tuple that can have any length
-    VaribleTuple = Tuple[Any, ...]
+    the second contains those that do not.'''
 
-    def reducer_func(accum: Tuple[VaribleTuple, VaribleTuple], el):
+    def reducer(accum: Tuple[VaribleTuple, VaribleTuple], el):
         if filter_func(el):
             return (*accum[0], el), accum[1]
 
         return accum[0], (*accum[1], el)
 
-    return reduce(reducer_func, subject, ((), ()))
+    return reduce(reducer, subject, ((), ()))

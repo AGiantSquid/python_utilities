7c1f58d230ca9f63f8abfd68621148349026f89c
AGiantSquid <ashley.e.shultz@gmail.com>
2021-03-16 13:26:33 -0700

    add get request helper

--- src/aws_utils/http_request.py
+++ src/aws_utils/http_request.py
@@ -1,11 +1,21 @@
 '''Module with functions for accepting requests from AWS lambda.'''
 import json
-from typing import Optional
+from typing import Optional, TypedDict
 
 from aws_utils.exceptions import BadRequest
 
 
-def get_body_from_post_request(event: dict):
+class PostRequest(TypedDict):
+    '''Dict representing an AWS event from a POST request.'''
+    body: Optional[str]
+
+
+class GetRequest(TypedDict):
+    '''Dict representing an AWS event from a GET request.'''
+    pathParameters: Optional[dict]
+
+
+def get_body_from_post_request(event: PostRequest):
     '''Get body from post request, and unmarshal the json value.
 
     Raise BadRequest if something is wrong.'''
@@ -20,3 +30,16 @@ def get_body_from_post_request(event: dict):
         raise BadRequest('Malformed json in request body')
 
     return body
+
+
+def get_params_from_get_request(event: GetRequest):
+    '''Get params from get request.
+
+    Raise BadRequest if something is wrong.'''
+
+    params = event.get('pathParameters')
+
+    if params is None:
+        raise BadRequest('Missing get parameters')
+
+    return params
